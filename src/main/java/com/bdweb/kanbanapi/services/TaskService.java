c5a6e4256687ae629ac726ddf43cfc286b5af63c
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-06-30 18:34:42 -0300

    Deixa de usar customer para usar board como atributo em task.

--- src/main/java/com/bdweb/kanbanapi/services/TaskService.java
+++ src/main/java/com/bdweb/kanbanapi/services/TaskService.java
@@ -1,9 +1,9 @@
 package com.bdweb.kanbanapi.services;
 
 import com.bdweb.kanbanapi.dtos.TaskRequest;
-import com.bdweb.kanbanapi.models.Customer;
+import com.bdweb.kanbanapi.models.Board;
 import com.bdweb.kanbanapi.models.Task;
-import com.bdweb.kanbanapi.repositories.CustomerRepository;
+import com.bdweb.kanbanapi.repositories.BoardRepository;
 import com.bdweb.kanbanapi.repositories.TaskRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,27 +11,26 @@ import javax.transaction.Transactional;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class TaskService {
 
     public final TaskRepository taskRepository;
-    public final CustomerRepository customerRepository;
+    public final BoardRepository boardRepository;
 
-    public TaskService(TaskRepository taskRepository, CustomerRepository customerRepository) {
+    public TaskService(TaskRepository taskRepository, BoardRepository boardRepository) {
         this.taskRepository = taskRepository;
-        this.customerRepository = customerRepository;
+        this.boardRepository = boardRepository;
     }
 
     @Transactional
-    public Task save(UUID id, TaskRequest request) {
+    public Task save(Long id, TaskRequest request) {
         Task task = new Task();
-        Optional<Customer> customerOptional = customerRepository.findById(id);
+        Optional<Board> boardOptional = boardRepository.findById(id);
         task.setTitle(request.getTitle());
         task.setDescription(request.getDescription());
         task.setRegistrationDate(ZonedDateTime.now());
-        task.setCustomer(customerOptional.get());
+        task.setBoard(boardOptional.get());
         return taskRepository.save(task);
     }
 
@@ -49,7 +48,7 @@ public class TaskService {
         Task task = new Task();
         task.setTitle(request.getTitle());
         task.setDescription(request.getDescription());
-        task.setCustomer(taskOptional.get().getCustomer());
+        task.setBoard(taskOptional.get().getBoard());
         task.setId(taskOptional.get().getId());
         task.setRegistrationDate(taskOptional.get().getRegistrationDate());
         return taskRepository.save(task);
