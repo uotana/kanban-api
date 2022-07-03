588c90437d85b4e351811e26c6cd7af2d275ef67
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-07-03 12:12:43 -0300

    Role implements GrantedAuthority.

--- src/main/java/com/bdweb/kanbanapi/models/Role.java
+++ src/main/java/com/bdweb/kanbanapi/models/Role.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.time.ZonedDateTime;
@@ -12,14 +13,19 @@ import java.time.ZonedDateTime;
 @Getter
 @Setter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", unique = true)
     private String name;
 
     @Column(name = "REGISTRATION_DATE")
     private ZonedDateTime registrationDate;
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
 }
