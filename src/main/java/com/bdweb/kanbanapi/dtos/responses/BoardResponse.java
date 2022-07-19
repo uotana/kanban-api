ac2f0629c97c0ef23fa58ccc4258f4fbd8814cdb
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-07-18 21:20:12 -0300

    Adiciona json naming com snake case strategy em BoardResponse

--- src/main/java/com/bdweb/kanbanapi/dtos/responses/BoardResponse.java
+++ src/main/java/com/bdweb/kanbanapi/dtos/responses/BoardResponse.java
@@ -1,5 +1,7 @@
 package com.bdweb.kanbanapi.dtos.responses;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,7 @@ import java.time.ZonedDateTime;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BoardResponse {
     private Long id;
     private String name;
