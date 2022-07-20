98acd623e85a74c975d35eebddafceafcb1f3e50
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-07-20 17:31:46 -0300

    Em BoardRepository findAllByCustomerId retorna list

--- src/main/java/com/bdweb/kanbanapi/repositories/BoardRepository.java
+++ src/main/java/com/bdweb/kanbanapi/repositories/BoardRepository.java
@@ -5,10 +5,10 @@ import com.bdweb.kanbanapi.models.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Optional<Board> findAllByCustomerId(UUID customerId);
+    List<Board> findAllByCustomerId(UUID customerId);
 }
